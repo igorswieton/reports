9dc83571f60bf035d6644094acac35611a7bd935
Igor Swieton <igorswtn@gmail.com>
2019-06-24 21:12:47 +0200

    set up gradle build file, add checkstyle file, add plugin main class

---
+++ spigot-reports/src/main/java/com/github/igorswieton/reportplugin/ReportPlugin.java
@@ -0,0 +1,33 @@
+package com.github.igorswieton.reportplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * @author Igor Swieton (https://www.github.com/igorswieton)
+ * @since 24.06.2019
+ */
+public final class ReportPlugin extends JavaPlugin {
+
+  /**
+   * Called when plugin is enabled.
+   */
+  @Override
+  public void onEnable() {
+    initialize();
+  }
+
+  /**
+   * Called when plugin is disabled.
+   */
+  @Override
+  public void onDisable() {
+
+  }
+
+  /**
+   * Serves to initialize variables from this class.
+   */
+  private void initialize() {
+
+  }
+}
